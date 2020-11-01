0c3e51c58dfe63b5b0035413d389a712683394f2
Nathaniel Peiffer <nathaniel@peiffer.com.au>
2020-11-01 20:48:08 +1100

    Update build

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,9 @@
-export * from './ipv4/ipv4';
-export * from './utils';
+export {SubnetRequirements, IPv4Network, Subnet} from './ipv4/ipv4';
+export {
+  CidrMaskSize,
+  CidrMaskToDottedDecimal,
+  DoRequirementsFit,
+  DottedDecimalToWildcard,
+  ParseIPv4Address,
+  RequirementsHostsCount,
+} from './utils';
